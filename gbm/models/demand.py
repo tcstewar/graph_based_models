d15f9b56253257325b47ffbbebd6161f2f5e5e80
Terry Stewart <tcstewar@uwaterloo.ca>
2014-11-23 01:54:22 -0500

    Adjusted scaling

--- gbm/models/demand.py
+++ gbm/models/demand.py
@@ -6,24 +6,24 @@ class DemandCurve(GraphBasedModel):
     def __init__(self):
         super(DemandCurve, self).__init__(name='Demand Curve',
                                     ylabel='price ($)',
-                                    xlabel='amount produced')
-        self.add(Parameter('p_max', 50, min=0, max=100,
+                                    xlabel='amount produced (%)')
+        self.add(Parameter('p_max', 5, min=0, max=20,
                            desc='Maximum Price'))
-        self.add(Parameter('p_min', 10, min=0, max=100,
+        self.add(Parameter('p_min', 0, min=0, max=20,
                            desc='Minimum Price'))
-        self.add(Parameter('slope', 0.1, min=0, max=5,
-                           desc='Price reduction'))
-        self.add(Parameter('quantity', 200, min=0, max=700,
+        self.add(Parameter('slope', 0, min=0, max=100,
+                           desc='Price reduction', decimals=2))
+        self.add(Parameter('quantity', 50, min=0, max=100,
                            desc='Quantity'))
 
     def generate_data(self, p):
         steps = 200
-        qq = np.linspace(0, 700, steps)
+        qq = np.linspace(0, 100, steps)
 
-        price = -p.slope * qq + p.p_max
+        price = -p.slope * p.p_max * 0.001 * qq + p.p_max
         price = np.maximum(price, p.p_min)
 
-        target_price = -p.slope * p.quantity + p.p_max
+        target_price = -p.slope * p.p_max * 0.001 * p.quantity + p.p_max
         target_price = np.maximum(target_price, p.p_min)
 
         results = [
